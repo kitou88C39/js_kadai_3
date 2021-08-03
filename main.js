be14863e288f5cc6870adf9e6caa57e421e3992f
kitou88C39 <kanfutrooper@icloud.com>
2021-08-03 09:37:56 +0900

    JS課題③ -2質問

--- main.js
+++ main.js
@@ -5,15 +5,29 @@ const newtask = document.getElementById('newtask');
 
 // 入力された値をリストに追加する
 const add = document.getElementById('add');
+//削除ボタンを押したときに何かの処理を実行したい
+//次にdeleteTask関数を作成し、deleteTaskの引数には削除したいタスクのIDを渡します
+//remove関数を呼び出してタスク(テーブルの一行の要素)を削除します
+const deleteTask = (taskId) => {
+ const task = document.getElementById('list');
+ task.remove();
+}
+//idの変数を用意
+let id = 0;
 
 add.addEventListener('click',() => {
-if(newtask.value =='') {
+  if(newtask.value =='') {
     return
-}
+  }
+  id += 1;
+
   // olタグを取得して変数に入れる
   const list = document.getElementById('list')
+  const tr = document.createElement('tr')
 
-  const id = list.children.length + 1;
+  const taskId = `task-${id}`
+  tr.id = taskId
+  //const id = list.children.length + 1;
   const comment = newtask.value;
 
   // 分かりやすい変数名をつけます
@@ -22,11 +36,16 @@ if(newtask.value =='') {
 
   const deleteButton = document.createElement('button');
   deleteButton.innerHTML = '削除';
+
+  //削除ボタンが押されたときにタスクを削除したいので
+//deleteButtonがクリックされたときにdeleteTaskを呼び出す
+
+  deleteButton.addEventListener('click', () => deleteTask(taskId));
   // テーブルの行に必要なデータの配列を作成します
   const cloumns = [id, comment, workButton, deleteButton]
 
   //　テーブルの行を作成します
-  const tr = document.createElement('tr');
+  // const tr = document.createElement('tr');
 
   // 配列の中身を一つずつ取り出して処理をします
   cloumns.forEach((cloumn) => {
@@ -41,11 +60,11 @@ if(newtask.value =='') {
   }
   // 行に列を追加します
     tr.appendChild(td);
+  
   })
   //テーブルに行を追加
   list.appendChild(tr);
-
-  newtask.value = ''
+  newtask.value = '' 
 })
 }
     
