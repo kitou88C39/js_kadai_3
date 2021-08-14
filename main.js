81172e0ec8ab5a2a87aca63e3243809145f6d4d8
kitou88C39 <kanfutrooper@icloud.com>
2021-08-14 16:31:46 +0900

    課題③ -4質問5回目

--- main.js
+++ main.js
@@ -75,6 +75,7 @@ const newtask = document.getElementById("newtask");
 // 入力された値をリストに追加する
 const add = document.getElementById("add");
 //削除ボタンを押したときに何かの処理を実行したい
+
 const deleteTask = (btn) => {
   const task = btn.closest("tr");
   const tbody = task.closest("tbody");
@@ -139,59 +140,61 @@ add.addEventListener("click", () => {
   newtask.value = "";
 });
 // チェックボックスの表示の切替
-function radioChange() {
-  const radio1_1 = document.getElementById("radio-all-select");
-  const radio1_2 = document.getElementById("radio-working-select");
-  const radio1_3 = document.getElementById("radio-done-select");
+// function radioChange() {
+//   const tasks = document.getElementsByName("list");
 
-  if (radio1_1.checked) {
-    tasks.slice();
-    return newTasks(tasks);
-  } else if (radio1_2.checked) {
-    const filterworking = tasks.filter((newtask) => {
-      return newtask.status === "作業中";
-    });
-    return newTasks(filterworking);
-  } else if (radio1_3.checked) {
-    const filterdone = tasks.filter((newtask) => {
-      return newtask.status === "完了";
-    });
-    return newTasks(filterdone);
-  }
-}
-// チェックボックスの表示の切替
-// const option = document.getElementsByName("p");
+//   const radio_1 = document.getElementById("radio-all-select");
+//   const radio_2 = document.getElementById("radio-working-select");
+//   const radio_3 = document.getElementById("radio-done-select");
 
-// const checkTasks = () => {
-//   const workingTasks = document.querySelectorAll(".work");
-//   const doneTasks = document.querySelectorAll(".finish");
-
-//   if (option[1].checked) {
-//     workingTasks.forEach((element) => {
-//       newtask.style.display = "";
+//function radioChange() {
+//   if (radio_1.checked) {
+//     tasks.slice();
+//     return newTasks(tasks);
+//   } else if (radio_2.checked) {
+//     const filterworking = tasks.filter((newtask) => {
+//       return newtask.status === "作業中";
 //     });
-//     doneTasks.forEach((element) => {
-//       newtask.style.display = "none";
+//     return newTasks(filterworking);
+//   } else if (radio_3.checked) {
+//     const filterdone = tasks.filter((newtask) => {
+//       return newtask.status === "完了";
 //     });
-//   } else if (option[2].checked) {
-//     doneTasks.forEach((element) => {
-//       newtask.style.display = "none";
-//     });
-//     doneTasks.forEach((element) => {
-//       newtask.style.display = "";
-//     });
-//   } else {
-//       progressTasks.forEach((element) => {
-//         element.style.display = "";
-//       });
-//       doneTasks.forEach((element) => {
-//         element.style.display = "";
-//       });
-//     }
-//   };
+//     return newTasks(filterdone);
+//   }
 // }
+// チェックボックスの表示の切替
+const option = document.getElementsByName("radio");
 
-//   // チェックボックスの表示の切替
+const checkTasks = () => {
+  const workingTasks = document.querySelectorAll(".work");
+  const doneTasks = document.querySelectorAll(".finish");
+
+  if (option[1].checked) {
+    workingTasks.forEach((workingTasks) => {
+      workingTasks.style.display = "";
+    });
+    doneTasks.forEach((doneTasks) => {
+      doneTask.style.display = "none";
+    });
+  } else if (option[2].checked) {
+    doneTasks.forEach((doneTasks) => {
+      doneTasks.style.display = "none";
+    });
+    workingTasks.forEach((workingTasks) => {
+      workingTasks.style.display = "";
+    });
+  } else {
+    workingTasks.forEach((workingTasks) => {
+      workingTasks.style.display = "";
+    });
+    doneTasks.forEach((doneTasks) => {
+      doneTask.style.display = "";
+    });
+  }
+};
+
+// チェックボックスの表示の切替
 
 //   function buttonClick() {
 //     const all = document.getElementById("r1");
